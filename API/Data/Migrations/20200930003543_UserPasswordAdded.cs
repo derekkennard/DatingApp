2d23fdf9e620fceccb059b82dab3b09092c76c8c
Derek Kennard <dkennard@homeinfusion.com>
2020-10-02 01:20:57 -0400

    End of Section 6

---
+++ API/Data/Migrations/20200930003543_UserPasswordAdded.cs
@@ -0,0 +1,45 @@
+﻿// Created by Derek Kennard (DerekKennard)
+// Solution: DatingApp
+// Project Name: API
+// File Name: 20200930003543_UserPasswordAdded.cs
+// Created on: 09/29/2020 at 8:35 PM
+// Edited on: 09/30/2020 at 11:32 PM
+// Developed and Copyrighted by ProHealth Pharmacy Solutions, LLC.
+
+#region imports
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#endregion
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                "PasswordHash",
+                "Users",
+                "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                "PasswordSalt",
+                "Users",
+                "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "PasswordHash",
+                "Users");
+
+            migrationBuilder.DropColumn(
+                "PasswordSalt",
+                "Users");
+        }
+    }
+}
